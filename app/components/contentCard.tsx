80e98fc66234365d074a0dc53cf37504db13c127
itpofy2024o <qweasdzxcf1G@proton.me>
2024-10-22 21:21:15 +0800

    idk

--- app/components/contentCard.tsx
+++ app/components/contentCard.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import "./contentCard.css";
 
+type Image = {
+    url: string;  // Assuming images is an array of objects with a 'url' field
+};
+
 type Post = {
-  id: number;
-  title: string;
-  createdAt: string;
-  tags: string[];
-  image: string;  // Assuming images is an array of objects with a 'url' field
+    id: number;
+    title: string;
+    createdAt: string;
+    tags: string[];
+    images: Image[]; // Images is now an array of Image objects
 };
 
 interface ContentCardProps {
@@ -22,10 +26,15 @@ const ContentCard: React.FC<ContentCardProps> = ({ post }) => {
     router.push(`/posts/${post.id}`); // Navigate to post detail page
   };
 
+  const firstImage = post.images.length > 0 
+  ? post.images[0].url 
+  : 
+  'https://wieck-mbusa-production.s3.amazonaws.com/photos/649b91bd0b9f17f65f17ff07630c6e4715681c4b/preview-928x522.jpg';
+
   return (
     <div className="content-card" onClick={handleCardClick}>
       <div className="content-card-image">
-        <img src={post.image} alt={post.title} />
+        <img src={firstImage} alt={post.title} />
       </div>
       <div className="content-card-details">
         <h2>{post.title}</h2>
