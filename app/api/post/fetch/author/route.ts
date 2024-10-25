040bf0321edf8ca00896f16f3ccaf5c176e342b2
itpofy2024o <hungaryisagoodforfornothungry@gmail.com>
2024-10-26 01:45:30 +0800

    pool-creator collection foxed

--- app/api/post/fetch/author/route.ts
+++ app/api/post/fetch/author/route.ts
@@ -1,9 +1,19 @@
 // /app/api/posts/by-creator/[username].ts
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
+import { URL } from 'url';
 
-export async function GET(request: Request, { params }: { params: { username: string } }) {
-  const { username } = params;
+export async function GET(request: Request) {
+  // Extract `username` from the query parameters of the request URL
+  const { searchParams } = new URL(request.url);
+  const username = searchParams.get("username");
+
+  if (!username) {
+    return NextResponse.json(
+      { message: "Username is required." },
+      { status: 400 }
+    );
+  }
 
   try {
     const posts = await prisma.content.findMany({
@@ -13,28 +23,24 @@ export async function GET(request: Request, { params }: { params: { username: st
           },
         },
         select: {
-          id: true,
-          title: true,
-          sold: true,
-          content: true,
-          medias: {
-            select: {
-              url: true,
-            },
-          },
-          createdAt: true,
-          updatedAt: true,
-          tags: {
-            select: {
-              name: true,
-            },
-          },
+            id: true,
+            title: true,
+            tags: { select: { name: true } }, // Fetch associated tags
+            medias: { select: { url: true } }, 
+            createdAt: true,
         },
         orderBy: {
           createdAt: 'desc', // Optional: Sort by creation date
         },
     });
-    return NextResponse.json(posts);
+    const formattedPosts = posts.map(post => ({
+        id: post.id,
+        title: post.title,
+        createdAt: post.createdAt,
+        tags: post.tags, // Keep the tags as an array of objects
+        image: post.medias.length > 0 ? post.medias[0].url : 'https://runnerstribe.com/wp-content/uploads/2024/08/femke-bol-with-team.jpg', // Get the first image or default image
+    }));
+    return NextResponse.json(formattedPosts);
   } catch (error) {
     return NextResponse.json(
       { message: "Error fetching posts by creator's username" },
