8019d2dbadbf50b0b38bf7023ad34498c95bc4da
itpofy2024o <hungaryisagoodforfornothungry@gmail.com>
2024-10-30 00:49:04 +0800

    finally ji

--- forum-backend/src/comment/comment.service.ts
+++ forum-backend/src/comment/comment.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { comment } from '@prisma/client';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+    constructor(private prisma: PrismaClient){}
+
+    async createCommentWithText(text: string, topicId: number): Promise<comment> {
+        return this.prisma.comment.create({
+            data: {
+                text: text,
+                topic: {connect:{id:topicId}}
+            },
+        });
+    }
+
+    async createCommentWithTextAndMedia(topicId:number): Promise<comment> {
+        return this.prisma.comment.create({
+            data: {
+                topic:{connect:{id:topicId}}
+            }
+        })
+    }
+
+
+    async createCommentWithMedia(topicId:number): Promise<comment> {
+        return this.prisma.comment.create({
+            data: {
+                topic:{connect:{id:topicId}}
+            }
+        })
+    }
+}
