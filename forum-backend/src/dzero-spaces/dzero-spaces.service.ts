e1f976a3b9c0b6c008a6f6057d3be9efeb4b919d
itpofy2024o <qweasdzxcf1G@proton.me>
2024-10-31 22:16:29 +0800

    multer error

--- forum-backend/src/dzero-spaces/dzero-spaces.service.ts
+++ forum-backend/src/dzero-spaces/dzero-spaces.service.ts
@@ -12,4 +12,50 @@ export class DzeroSpacesService {
     constructor(private readonly spacesConfig: SpacesConfig) {
         this.s3 = this.spacesConfig.getS3();
     }
+
+    async uploadFile(file: Express.Multer.File, filePath: string): Promise<string> {
+        const params = {
+            Bucket: this.bucketName,
+            Key: filePath,
+            Body: file.buffer,
+            ACL: "public-read",
+            ContentType: file.mimetype,
+        };
+
+        try {
+            const { Location } = await this.s3.upload(params).promise();
+            return Location;
+        } catch (error) {
+            console.error('Error uploading to DigitalOcean Space:', error);
+            throw new Error('File upload failed');
+        }
+    }
+
+    async getFileUrl(filePath: string): Promise<string> {
+        const params = {
+            Bucket: this.bucketName,
+            Key: filePath,
+            Expires: 60*60,
+        };
+        try {
+            return this.s3.getSignedUrlPromise('getObject', params);
+        } catch (error) {
+            console.error('Error generating pre-signed URL:', error);
+            throw new Error('Unable to get file URL');
+        }
+    }
+
+    async deleteFile(filePath: string): Promise<void> {
+        const params = {
+            Bucket: this.bucketName,
+            Key: filePath,
+        };
+
+        try {
+            await this.s3.deleteObject(params).promise();
+        } catch (error) {
+            console.error('Error deleting file:', error);
+            throw new Error('File deletion failed');
+        }
+    }
 }
