15b6b158c0e9c218bb9dd760bd57cca623161d9b
itpofy2024o <qweasdzxcf1G@proton.me>
2024-11-03 03:14:40 +0800

    localstorage + filter form + individual subforum page list of all topics with check list filter to select topics for listing

--- forum-backend/src/subforum/subforum.controller.ts
+++ forum-backend/src/subforum/subforum.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Query, Body, ConflictException } from '@nestjs/common';
 import { SubforumService } from './subforum.service';
 
 @Controller('subforum')
@@ -16,5 +16,14 @@ export class SubforumController {
         @Body("description") description: string
     ) {
         return this.subforumService.createOneSubForum(name,description)
+    }
+
+    @Get("filter")
+    async findSlect(
+        @Query('name') name?: string,
+        @Query('createdAt') createdAt?: string,
+    ) {
+        const parsedDate = createdAt ? new Date(createdAt) : undefined;
+        return this.subforumService.getAllSubForumsThatFitFilter(name, parsedDate);
     }
 }
